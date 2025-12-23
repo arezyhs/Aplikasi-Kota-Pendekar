ccc40286056136736fa2b4886fe699a2327d9090
Akbar Setiyawan <89913761+arezyhs@users.noreply.github.com>
2025-12-24 04:51:30 +0700

    fix splash screen

--- lib/homepage/views/splash/splashscreen.dart
+++ lib/homepage/views/splash/splashscreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:pendekar/homepage/size_config.dart';
 import 'package:pendekar/homepage/views/home/home_view.dart';
 
